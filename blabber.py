1f7b8594c308508a96bade1f7afa3a1a6813c4be
Graeme O <graeme.ong>
2018-03-31 00:13:36 +1100

    [GraemeO] Add Google speech API integration

--- blabber.py
+++ blabber.py
@@ -1,6 +1,14 @@
 from snowboy import snowboydecoder
 import signal
 import subprocess
+import io
+import time
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+from threading import Thread
 
 interrupted = False
 
@@ -13,13 +21,40 @@ def interrupt_callback():
     return interrupted
 
 def record_callback():
+    record_thread = Thread(target=record())
+    record_thread.start()
+    record_thread.join()
+
+    stt_thread = Thread(target=speech_to_text())
+    stt_thread.start()
+    stt_thread.join()
+
+
+def record():
     snowboydecoder.play_audio_file()
     print "Recording..."
 
     command = "rec -r 16000 -b 16 resources/capture.raw silence 1 0.1 3% 1 3.0 3%"
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).wait()
 
+def text_to_speech():
+    subprocess.Popen("espeak 'yes' -s150", stdout=subprocess.PIPE, shell=True).wait()
 
+def speech_to_text():
+    client = speech.SpeechClient()
+    file_name = "/Users/graemeong/Documents/projects/blabber/resources/capture.raw"
+    
+    with io.open(file_name, 'rb') as audio_file:
+         content = audio_file.read()
+         audio = types.RecognitionAudio(content=content)
+    
+    config = types.RecognitionConfig(encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16, sample_rate_hertz=16000, language_code='en-US')
+    
+    response = client.recognize(config, audio)
+    
+    for result in response.results:
+         print('Transcript: {}'.format(result.alternatives[0].transcript))
+    
 def hotword_detector():
     model = "resources/Hi_Sally.pmdl"
     
@@ -29,7 +64,7 @@ def hotword_detector():
     detector.start(detected_callback=record_callback,
                    interrupt_check=interrupt_callback,
                    sleep_time=0.03)
-    
+
     detector.terminate()
 
 def main():
